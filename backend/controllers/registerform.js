5136e69a453a58c537c04ebdef110145962a105a
Santhosh M <sandymoo1523@gmail.com>
2025-08-10 21:53:48 +0530

    updated code

--- backend/controllers/registerform.js
+++ backend/controllers/registerform.js
@@ -1,11 +1,16 @@
-const registerformodal = require("../models/registerform")
+const registerformodal = require("../models/registerform");
 
-exports.registerformController=async(req,res,next)=>{
-    try{
+exports.registerformController = async (req, res) => {
+    try {
+        console.log("Incoming request body:", req.body); // debug log
 
-        const {firstname,mobile,email,password,address,city,state,zipcode,country} = req.body
-            
-        const registerformdetails=await registerformodal.create({
+        const { firstname, mobile, email, password, address, city, state, zipcode, country } = req.body;
+
+        if (!firstname || !email) {
+            return res.status(400).json({ message: "Firstname and Email are required" });
+        }
+
+        const registerformdetails = await registerformodal.create({
             firstname,
             mobile,
             email,
@@ -15,19 +20,18 @@ exports.registerformController=async(req,res,next)=>{
             state,
             zipcode,
             country
-        })
-        res.json({
-            message:"registeration successful",     
-            registerformdetails
-        })
+        });
 
+        res.status(201).json({
+            message: "Registration successful",
+            data: registerformdetails
+        });
+
+    } catch (error) {
+        console.error("Registration error:", error);
+        res.status(500).json({
+            message: "Internal Server Error",
+            error: error.message
+        });
     }
-    catch(error){
-    console.error("registration error", error);
-    res.status(500).json({
-        message: "Internal Server Error",
-        error: error.message
-    });
-}
-
-}
+};
