8e7a4d072ceb9f74a8087709cb31f36c0df0a70d
Santhosh M <sandymoo1523@gmail.com>
2025-08-13 14:36:35 +0530

    lastest code

--- backend/controllers/registerMobileController.js
+++ backend/controllers/registerMobileController.js
@@ -1,11 +1,12 @@
 const userRegisterMobileModal = require("../models/registerMobile");
 const axios = require("axios");
+const { route } = require("../routes/user");
 
 let storeOTP = {};
+const FAST2SMS_API_KEY='jVCW6qgdyI0RHZeFY7wAh2Q1tBLnpfxrJPNoU98SD4aObcT3Gst5fnSO0ZJmGxUlk7YsoKvIVXCwcRyu'
 
 exports.registerMobileController = async (req, res, next) => {
      console.log("📩 Request received:", req.body);
-     console.log("API Key present locally?", !!process.env.FAST2SMS_API_KEY);
   try {
     const { mobile } = req.body;
     if (!mobile || mobile.length !== 10) {
@@ -14,27 +15,29 @@ exports.registerMobileController = async (req, res, next) => {
 
     // Save mobile in DB
     const MobileRegister = await userRegisterMobileModal.create({ mobile });
-
+ 
     // Generate OTP
     const otp = Math.floor(100000 + Math.random() * 900000);
     storeOTP[mobile] = { otp, expires: Date.now() + 5 * 60 * 1000 };
 
+    const smsData={
+        message: `Welcome to zipvikz! ${otp} is your OTP for logging into https://zipvikzin.vercel.app/ Thank You - zipvikz Brand`,
+        language:"english",
+        route:"q",
+        numbers:mobile
+    }
+
     // Send OTP using GET
-    const response = await axios.get("https://www.fast2sms.com/dev/bulkV2", {
-      params: {
-        variables_values: otp,
-        route: "otp",
-        numbers: mobile
-      },
+    const response = await axios.post("https://www.fast2sms.com/dev/bulkV2",smsData {
       headers: {
-        authorization: process.env.FAST2SMS_API_KEY
+        authorization: FAST2SMS_API_KEY,
+        'Content-Type':"application/json"
       }
     });
 
     if (response.data.return) {
       res.json({
         message: "OTP sent successfully",
-        otpSent: true,
         MobileRegister
       });
     } else {
