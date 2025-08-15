1187802a75d9dabd122302c872f59cfe2c558dd0
Santhosh M <sandymoo1523@gmail.com>
2025-08-15 14:55:21 +0530

    lastest code

--- backend/controllers/registerMobileController.js
+++ backend/controllers/registerMobileController.js
@@ -1,5 +1,6 @@
 const userRegisterMobileModal = require("../models/registerMobile");
 exports.registerMobileController = async (req, res, next) => {
+    console.log("Mobile received from frontend:", mobile);
 
     try {
         const { mobile } = req.body
