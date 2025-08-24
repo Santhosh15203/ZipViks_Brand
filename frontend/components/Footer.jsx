15afdfd0af2e0f1d69d0a77199b03136825d6e08
Santhosh M <sandymoo1523@gmail.com>
2025-08-24 12:22:00 +0530

    lastest code

--- frontend/components/Footer.jsx
+++ frontend/components/Footer.jsx
@@ -20,17 +20,17 @@ export default function Footer() {
                    <span className="text-danger fw-bold">Online</span>  shopping has never been this easy, stylish, and fun.</p>
               </div>
 
-               <div className="" >
+               <div className="bg-success" >
                   <p className="text-decoration-underline justify-content-center d-flex  text-white mb-0">PRODUCTS</p>
                    <ul className="list-unstyled gap-2 gap-lg-1 justify-content-center d-flex flex-wrap flex-lg-column mb-0 mb-lg-1 ">
-                    <li><a href="#" className="text-decoration-none  text-white">TShirts</a></li>
-                    <li><a href="#" className="text-decoration-none text-white ">Hoodies</a></li>
-                    <li><a href="#" className="text-decoration-none text-white ">TrackPant</a></li>
-                    <li><a href="#" className="text-decoration-none text-white ">Shorts</a></li>
-                    <li><a href="#" className="text-decoration-none  text-white ">Chudi</a></li>
-                    <li><a href="#" className="text-decoration-none  text-white">Leggins</a></li>
-                    <li><a href="#" className="text-decoration-none  text-white ">Saree</a></li>
-                    <li><a href="#" className="text-decoration-none  text-white ">NightWear</a></li>
+                    <li className="mb-0"><a href="#" className="text-decoration-none  text-white">TShirts</a></li>
+                    <li className="mb-0"><a href="#" className="text-decoration-none text-white ">Hoodies</a></li>
+                    <li className="mb-0"><a href="#" className="text-decoration-none text-white ">TrackPant</a></li>
+                    <li className="mb-0"><a href="#" className="text-decoration-none text-white ">Shorts</a></li>
+                    <li className="mb-0"><a href="#" className="text-decoration-none  text-white ">Chudi</a></li>
+                    <li className="mb-0"><a href="#" className="text-decoration-none  text-white">Leggins</a></li>
+                    <li className="mb-0"><a href="#" className="text-decoration-none  text-white ">Saree</a></li>
+                    <li className="mb-0"><a href="#" className="text-decoration-none  text-white ">NightWear</a></li>
                   </ul>
                   
               </div>
@@ -58,7 +58,7 @@ export default function Footer() {
                <div className="" >
                   <p className="text-decoration-underline justify-content-center  d-flex m-0 p-0  text-white mb-0">CONTACT US</p>
 
-                   <div className="list-unstyled flex-wrap d-flex justify-content-between flex-lg-column gap-1 gap-lg-0 ">
+                   <div className="list-unstyled flex-wrap d-flex justify-content-center flex-lg-column gap-1 gap-lg-0 ">
                     <div className="">
                       <p className="mb-1 m-0" ><a href="#" className="text-decoration-none  text-white"><i class="bi bi-envelope-at"></i>  zipvikzshoppy@gmail.com</a></p>
                       <p className="m-0 p-0"><i class="bi bi-telephone-outbound"></i> +91 99999 99991</p>
